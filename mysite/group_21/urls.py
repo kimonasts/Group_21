02bcbb72c799ab1fd5a01572cc92762138ab01ae
kimonasts <75432230+kimonasts@users.noreply.github.com>
2022-02-28 14:10:37 +0000

    Add files via upload

---
+++ mysite/group_21/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('welcome/', views.welcome, name='welcome'),
+    path('email_sign_in/', views.email_sign_in, name='email_sign_in'),
+    path('home/', views.home, name='home'),
+    path('reset_password/', views.reset_password, name='reset_password'),
+    path('username_sign_in/', views.username_sign_in, name='username_sign_in'),
+    path('registration/', views.registration, name='registration'),
+]
---
+++ mysite/group_21/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request):
+    return render(request, 'group_21/welcome.html')
+
+
+def welcome(request):
+    return render(request, 'group_21/welcome.html')
+
+
+def email_sign_in(request):
+    return render(request, 'group_21/email_sign_in.html')
+
+def home(request):
+    return render(request, 'group_21/home.html')
+
+def reset_password(request):
+    return render(request, 'group_21/reset_password.html')
+
+
+def username_sign_in(request):
+    return render(request, 'group_21/username_sign_in.html')
+
+
+def registration(request):
+    return render(request, 'group_21/registration.html')
